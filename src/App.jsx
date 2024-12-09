c1c13afc0d25a6064519d7f173c35134d278b529
Julio Hernandez, Jr. <132781123+JunoZeno@users.noreply.github.com>
2024-12-08 21:52:21 -0500

    Add PizzaOfTheDay component and integrate it into App (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import PizzaOfTheDay from "./PizzaOfTheDay";
 import Order from "./Order";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
       <div>
         <h1>Padres Gino's - Order Now</h1>
         <Order></Order>
+        <PizzaOfTheDay></PizzaOfTheDay>
       </div>
     </StrictMode>
   );
