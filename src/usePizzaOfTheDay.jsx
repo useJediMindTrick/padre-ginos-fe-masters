67b643e51e03d78abecc4a55dcd9d4b0309fbb0a
JunoZeno <juliohernandez235@gmail.com>
2024-12-08 22:14:01 -0500

    Enhance App layout with logo class and improve Order component structure

--- src/usePizzaOfTheDay.jsx
+++ src/usePizzaOfTheDay.jsx
@@ -1,7 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useDebugValue } from "react";
 
 export const usePizzaOfTheDay = () => {
   const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+  useDebugValue(
+    pizzaOfTheDay
+      ? `${pizzaOfTheDay.id} : ${pizzaOfTheDay.name} `
+      : "loading...",
+  );
 
   useEffect(() => {
     async function fetchPizzaOfTheday() {
