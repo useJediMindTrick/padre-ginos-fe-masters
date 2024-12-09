27c973ce2107be50be39fc2c5116dfd265e9000d
JunoZeno <juliohernandez235@gmail.com>
2024-12-08 21:22:34 -0500

    Add PizzaOfTheDay component and integrate it into App

---
+++ src/usePizzaOfTheDay.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export const usePizzaOfTheDay = () => {
+  const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+
+  useEffect(() => {
+    async function fetchPizzaOfTheday() {
+      const response = await fetch("/api/pizza-of-the-day");
+      const data = await response.json();
+      setPizzaOfTheDay(data);
+    }
+
+    fetchPizzaOfTheday();
+  }, []);
+
+  return pizzaOfTheDay;
+};
