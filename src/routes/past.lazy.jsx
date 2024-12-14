643cf3badecf87140f71a8aeaa3aec222610105e
useJediMindTrick <julio.developerx@gmail.com>
2024-12-14 15:07:22 -0500

    Add lazy variable to /past route for improved code clarity

--- src/routes/past.lazy.jsx
+++ src/routes/past.lazy.jsx
@@ -3,3 +3,5 @@ import { createLazyFileRoute } from '@tanstack/react-router'
 export const Route = createLazyFileRoute('/past')({
   component: () => <div>Hello /past!</div>,
 })
+
+const lazy = "lazy"
