7ac5a8e3f424d17b90a9a062aeb9c657c9cd6965
useJediMindTrick <julio.developerx@gmail.com>
2024-12-14 20:53:00 -0500

    Refactor API structure: migrate SQLite queries, implement getPastOrders function, and update route handling with React Query

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import PizzaOfTheDay from "../PizzaOfTheDay";
 import Header from "../Header";
 import { CartContext } from "../contexts";
@@ -18,6 +19,7 @@ export const Route = createRootRoute({
           </div>
         </CartContext.Provider>
         <TanStackRouterDevtools />
+        <ReactQueryDevtools />
       </>
     );
   },
--- src/routes/past.lazy.jsx
+++ src/routes/past.lazy.jsx
@@ -1,5 +1,15 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { createLazyFileRoute } from "@tanstack/react-router";
+import getPastOrders from "../api/getPastOrders";
 
-export const Route = createLazyFileRoute('/past')({
-  component: () => <div>Hello /past!</div>,
-})
+export const Route = createLazyFileRoute("/past")({
+  component: PastOrdersRoute,
+});
+
+function PastOrdersRoute() {
+  const [page, setPage] = useState(1);
+  const { isLoading, data } = useQuery({
+    queryKey: ["past-orders", page],
+  });
+}
