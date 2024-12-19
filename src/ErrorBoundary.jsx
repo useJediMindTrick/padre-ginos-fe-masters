3f33fec1dc54ffc2a54259b4da4d0725f396f28e
Julio Hernandez, Jr. <132781123+useJediMindTrick@users.noreply.github.com>
2024-12-19 17:44:11 -0500

    Error-boundaries-class-components (#7)
    * Add ErrorBoundary component to handle errors in the application
    
    * Wrap PastOrdersRoute with ErrorBoundary for improved error handling

---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,31 @@
+import { Component } from "react";
+import { Link } from "@tanstack/react-router";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+  componentDidCatch(error, errorInfo) {
+    console.error("ErrorBoundary caught some stupid error", error, info);
+  }
+
+  componentDidMount() {}
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="error-boundary">
+          <h2>Uh oh!</h2>
+          <p>
+            The was an error with this page.
+            <Link>Click here </Link> to go to home page.
+          </p>
+        </div>
+      );
+    }
+    return this.props.children; // this just passes the child component if there are not errors
+  }
+}
+
+export default ErrorBoundary;
