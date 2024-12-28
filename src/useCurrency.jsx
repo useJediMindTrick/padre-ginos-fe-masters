9c6d49a04fccf48ef899e2df35be3ca79454b33b
Julio Hernandez, Jr. <132781123+useJediMindTrick@users.noreply.github.com>
2024-12-27 21:42:32 -0500

    Refactor Prettier configuration, add ignore file, clean up ErrorBoundary, update package.json for coverage, and add Cart component tests with snapshots (#10)

--- src/useCurrency.jsx
+++ src/useCurrency.jsx
@@ -1,6 +1,6 @@
-const intl = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
+const intl = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
 });
 export const priceConverter = (price) => intl.format(price);
 export default function useCurrency(price) {
